fe47ec2b096f54bf0ae761e7e7e954a2629de3f7
franciscoperezfuentes2004-tech <franciscoperezfuentes2004@gmail.com>
2026-05-26 22:31:10 -0600

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const { db } = require('./api/config/db.js');
-db.collection('ajustes').doc('global').get()
-  .then(doc => { console.log(JSON.stringify(doc.data(), null, 2)); process.exit(0); })
-  .catch(console.error);
